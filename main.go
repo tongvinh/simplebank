b43862d2a095e88b6d5f9bec314b3c2de428af28
Vinh Tong <tonggiavinh2010@gmail.com>
2023-01-09 15:18:56 +0700

    initial simplebank

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"github.com/tongvinh/simplebank/api"
+	db "github.com/tongvinh/simplebank/db/sqlc"
+	"github.com/tongvinh/simplebank/util"
+	"log"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
